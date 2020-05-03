52f84dc3c4e2f2105e1b083885e0772190e0a084
Shubham Gupta( s0641) <48442627+s0641@users.noreply.github.com>
2020-05-03 13:16:06 +0530

    Add files via upload

---
+++ QuestionsActivity.java
@@ -0,0 +1,135 @@
+package com.app.quiz;
+
+import android.content.Intent;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class QuestionsActivity extends AppCompatActivity {
+    TextView tv;
+    Button submitbutton, quitbutton;
+    RadioGroup radio_g;
+    RadioButton rb1,rb2,rb3,rb4;
+
+    String questions[] = {
+            "1. How many bones are there in your body?",
+            "2. Where was Lord Krishna born?",
+            "3. Name the indian states with maximum number of airports?",
+            "4. Fungi are plants that lack.......?",
+            "5. When did Indian Football team first appeared in Olympics?",
+            "6. Human skull is made of how many bones?",
+            "7. Where is Disney land located?",
+            "8. The First atom bomb was dropped on which city?",
+            "9. What is glass mainly made of ?",
+            "10. A byte is equivalent to --- bits?"
+    };
+    String answers[] = {"206","Mathura","Gujarat","Chlorophyll","1948","22","California","Hiroshima","Sand","8"};
+    String opt[] = {
+            "206","306","207","307",
+            "Haridwar","Mumbai","Varanasi","Mathura",
+            "Bombay","Gujarat","Kolkata","Delhi",
+            "Color","Oxygen","Chlorophyll","Water",
+            "1948","1952","1956","1990",
+            "30","22","26","18",
+            "Texas","California","Chicago","None of the mentioned",
+            "Beijing","Islamabad","Hiroshima","Russia",
+            "Paper","Sand","Stone","Steel",
+            "4","8","6","2"
+    };
+    int flag=0;
+    public static int marks=0,correct=0,wrong=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questions);
+
+        final TextView score = (TextView)findViewById(R.id.textView4);
+        TextView textView=(TextView)findViewById(R.id.DispName);
+        Intent intent = getIntent();
+        String name= intent.getStringExtra("myname");
+
+        if (name.trim().equals(""))
+            textView.setText("Hello User");
+        else
+            textView.setText("Hello " + name);
+
+        submitbutton=(Button)findViewById(R.id.button3);
+        quitbutton=(Button)findViewById(R.id.buttonquit);
+        tv=(TextView) findViewById(R.id.tvque);
+
+        radio_g=(RadioGroup)findViewById(R.id.answersgrp);
+        rb1=(RadioButton)findViewById(R.id.radioButton);
+        rb2=(RadioButton)findViewById(R.id.radioButton2);
+        rb3=(RadioButton)findViewById(R.id.radioButton3);
+        rb4=(RadioButton)findViewById(R.id.radioButton4);
+        tv.setText(questions[flag]);
+        rb1.setText(opt[0]);
+        rb2.setText(opt[1]);
+        rb3.setText(opt[2]);
+        rb4.setText(opt[3]);
+        submitbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //int color = mBackgroundColor.getColor();
+                //mLayout.setBackgroundColor(color);
+
+                if(radio_g.getCheckedRadioButtonId()==-1)
+                {
+                    Toast.makeText(getApplicationContext(), "Please select one choice", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                RadioButton uans = (RadioButton) findViewById(radio_g.getCheckedRadioButtonId());
+                String ansText = uans.getText().toString();
+//                Toast.makeText(getApplicationContext(), ansText, Toast.LENGTH_SHORT).show();
+                if(ansText.equals(answers[flag])) {
+                    correct++;
+                    Toast.makeText(getApplicationContext(), "Correct", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    wrong++;
+                    Toast.makeText(getApplicationContext(), "Wrong", Toast.LENGTH_SHORT).show();
+                }
+
+                flag++;
+
+                if (score != null)
+                    score.setText(""+correct);
+
+                if(flag<questions.length)
+                {
+                    tv.setText(questions[flag]);
+                    rb1.setText(opt[flag*4]);
+                    rb2.setText(opt[flag*4 +1]);
+                    rb3.setText(opt[flag*4 +2]);
+                    rb4.setText(opt[flag*4 +3]);
+                }
+                else
+                {
+                    marks=correct;
+                    Intent in = new Intent(getApplicationContext(),ResultActivity.class);
+                    startActivity(in);
+                }
+                radio_g.clearCheck();
+            }
+        });
+
+        quitbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),ResultActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
